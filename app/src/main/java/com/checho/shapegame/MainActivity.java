cd6a41220e53bdb0d79f2f367ca947fe59304ce8
checho <sergioestebanramirez@yahoo.es>
2019-08-27 11:34:11 -0500

    Se añade la carpeta Release con el APK

--- app/src/main/java/com/checho/shapegame/MainActivity.java
+++ app/src/main/java/com/checho/shapegame/MainActivity.java
@@ -1,31 +1,23 @@
 package com.checho.shapegame;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
-
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-
 import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
-
 import android.widget.Button;
-
-
 import android.widget.ImageView;
-
-
 import android.widget.TextView;
-
-
 import java.util.Random;
 
+/**
+ * @author Checho
+ */
 public class MainActivity extends AppCompatActivity {
 
 
